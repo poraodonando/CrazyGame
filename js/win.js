ef11a4feead478ab8cde868fc6a2386cafaa6b2d
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2023-09-21 17:45:07 -0300

    up

--- js/win.js
+++ js/win.js
@@ -1,10 +1,19 @@
-var form = document.getElementById('formulario');
-var campo = document.getElementById('campo');
+// Função que mostra o valor do input num alert
+function mostrarValor() {
+    alert(document.getElementById("meu-input").value);
+}
 
-form.addEventListener('submit', function(e) {
-    // alerta o valor do campo
-    alert(campo.value);
+// Evento que é executado toda vez que uma tecla for pressionada no input
+document.getElementById("meu-input").onkeypress = function(e) {
+    // 13 é a tecla <ENTER>. Se ela for pressionada, mostrar o valor
+    if (e.keyCode == 13) {
+        mostrarValor();
+        e.preventDefault();
+    }
+}
 
-    // impede o envio do form
+// Evento que é executado ao clicar no botão de enviar
+document.getElementById("meu-submit").onclick = function(e) {
+    mostrarValor();
     e.preventDefault();
-});
+}
